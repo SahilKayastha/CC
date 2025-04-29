4dcb432fc73009e06d145b811d61ecd6683fc5ea
SahilKayastha <120790086+SahilKayastha@users.noreply.github.com>
2025-04-29 13:46:04 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,15 @@
+print("This is python program")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter Second number: "))
+c = input("Enter Operation: ")
+
+if(c=='+'):
+  print(a+b)
+elif(c == '-'):
+  print(a-b)
+elif (c=='*'):
+  print(a*b)
+elif (c=='/'):
+  print(a/b)
+
