b2fd85923e038eea78d7a7d82c7bb07ca9e7a813
SahilKayastha <120790086+SahilKayastha@users.noreply.github.com>
2025-04-29 13:42:53 +0530

    Create cal.py

---
+++ cal.py
@@ -0,0 +1,15 @@
+print("This is python program")
+
+a = input("Enter first number: ")
+b = input("Enter Second number: ")
+c = input("Enter Operation")
+
+if(c=='+'):
+  print(a+b)
+elif(c == '-'):
+  print(a-b)
+elif (c=='*'):
+  print(a*b)
+elif (c=='/'):
+  print(a/b)
+
